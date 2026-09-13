af4e1cb3be6ec0129818767b9964c6494b99d80f
Adrian Estrada Jr <adrianestrada497@gmail.com>
2026-09-13 16:24:04 -0500

    Added TODOs into account_security

--- lib/account_security.rb
+++ lib/account_security.rb
@@ -2,14 +2,17 @@ class AccountSecurity
 
     # Allows the user to login in 
     def login(username, password)
+        #TODO: Implement login logic
     end
 
     # The user can enter their recovery phrase in order to update their password
     def AccountRecovery(username, recoveryPhrase)
+        #TODO: Implement account recovery using recovery phrase
     end
 
     # Creates and stores a recovery phrase for the user
     def recoveryPhraseCreation(username)
+        #TODO: Implement logic for generating a recovery phrase
     end
 
 
