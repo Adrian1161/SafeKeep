630e82a85db45a6993c930bcc558a8c877baae53
Adrian Estrada Jr <adrianestrada497@gmail.com>
2026-09-12 19:41:42 -0500

    Added pairing log entry and set up account_security methods

---
+++ docs/pairing_log.md
@@ -0,0 +1,11 @@
+# Pairing Log 
+
+Session 2 - 2026-09-12
+
+Driver: Adrian
+Navigator: Philip
+
+Work completed
+- Defined Account Security class
+
+=end 
---
+++ lib/account_security.rb
@@ -0,0 +1,15 @@
+class AccountSecurity
+
+    # Responsible for logging in user
+    def login(username, password)
+    end
+
+    # Allows the user to type in their recovery Phrase and change their password
+    def accountRecovery(username, recoveryPhrase)
+    end 
+
+    # Creates a recovery phrase for the user
+    def recoveryPhraseCreation(username)
+    end
+
+end
