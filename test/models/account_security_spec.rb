4b0f00d36188fd937f4aa075295f34c6202fac3b
Adrian Estrada Jr <adrianestrada497@gmail.com>
2026-09-14 17:57:10 -0500

    Removed place holder code from account_security_spec.rb

--- test/models/account_security_spec.rb
+++ test/models/account_security_spec.rb
@@ -3,8 +3,5 @@ require_relative "../../lib/account_security"
 require "sqlite3"
 
 RSpec.describe AccountSecurity do
-    it "logs in with valid credentials" do
-        account_security = AccountSecurity.new
-        db = SQLite3::Database.new(":memory:")
-    end
+  
 end
