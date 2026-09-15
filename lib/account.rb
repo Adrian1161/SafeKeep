fdbef6f887d2a73bb2f2555303da2ae4e1ff8dc7
Philip Savov <phsavov@gmail.com>
2026-09-14 23:53:05 -0500

    Finishing Story 1 for account creation, as well as updating account module based on rails functionality.

--- lib/account.rb
+++ lib/account.rb
@@ -1,74 +1,12 @@
 # account.rb
-
+require 'bcrypt'
 =begin
-This file contains the Account class, which represents a user account with attributes such as username, password, recovery phrase, and PIN.
-The class includes methods for encrypting and decrypting passwords, fetching account details from a database, and updating account details in the database.
+Since we are using rails, and defined the table structure in account_schema.rb, the getters and setters are handled by rails.
+So we only need to define the validations and associations.
 TODO comments have been added to indicate areas that need implementation.
 =end
-class Account
-  # Attributes needed for account
-  attr_accessor :username, :password, :recoveryPhrase, :pin
+class Account < ApplicationRecord
+  has_secure_password
 
-  # Method for creating Account
-  def initialize(username, password, recoveryPhrase, pin)
-    @username = username
-    @password = encryptPassword(password)
-    @recoveryPhrase = recoveryPhrase
-    @pin = pin
-  end
-
-  # Method for encrypting password
-  def encryptPassword(password)
-    # TODO: Implement encryption logic
-  end
-
-  # Method for encrypting password
-  def decryptPassword(password)
-    # TODO: Implement decryption logic
-  end
-
-  # Method for fetching account details from the database
-  def fetchAccountDetailsFromDatabase
-    # TODO: Implement fetching account details from database
-  end
-
-  # Method for updating account details in the database
-  def updateAccountDetailsFromDatabase
-    # TODO: Implement updating account details in database
-  end
-
-  # Adding Getters and Setters
-  def username
-    @username
-  end
-
-  def password
-    @password
-  end
-
-  def recoveryPhrase
-    @recoveryPhrase
-  end
-
-  def pin
-    @pin
-  end
-
-  # TODO: For setters, need to update in DB when any of the attributes are updated
-  def recoveryPhrase=(new_recoveryPhrase)
-    @recoveryPhrase = new_recoveryPhrase
-  end
-
-  def pin=(new_pin)
-    @pin = new_pin
-  end
-
-  def password=(new_password)
-    @password = new_password
-  end
-
-  def username=(new_username)
-    @username = new_username
-  end
-
+  validates :username, presence: true, uniqueness: true
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,20 +1,32 @@
 # frozen_string_literal: true
 
-require 'rspec'
-require_relative '../../lib/account'
-
+require 'rspec/rails'
+require_relative '../../config/environment'
+#Test cases for Account model
 RSpec.describe 'Account' do
-  before do
-    # Do nothing
-  end
-
-  after do
-    # Do nothing
-  end
-
-  context 'when condition' do
-    it 'succeeds' do
-      pending 'Not implemented'
+  describe 'Account' do
+    it "should be defined" do
+      expect {Account}.not_to raise_error
+    end
+    describe "created an account" do
+      before(:each) do
+        @account = Account.new(
+          username: 'test',
+          password: 'test1234'
+        )
+      end
+      it "should set the username" do
+        expect(@account.username).to eq('test')
+      end
+      it "should accept a valid username and password" do
+        expect(@account).to be_valid
+      end
+    end
+    describe "username validation" do
+      it "should reject an account without a username" do
+        account = Account.new(password: 'test1234')
+        expect(account).not_to be_valid
+      end
     end
   end
 end
